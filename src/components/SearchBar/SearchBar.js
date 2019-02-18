97bbf7835bad7e849fd8a72b91a71f80a6643acf
macjabeth <macbethjonathan@gmail.com>
2019-02-18 23:19:18 +0000

    implement searchbar component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './SearchBar.css';
+
+const SearchBar = () => (
+  <header>
+    <div>
+      <i className="fab fa-instagram"></i>
+      <h1>Instagram</h1>
+    </div>
+    <div>
+      <input type="search" placeholder="Search" aria-label="Search through content" />
+    </div>
+    <div className="nav">
+      <i className="far fa-compass"></i>
+      <i className="far fa-heart"></i>
+      <i className="far fa-user"></i>
+    </div>
+  </header>
+);
+
+export default SearchBar;
